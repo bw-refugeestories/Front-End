52752acae2736e39a3ecfc52edc40bd83f312521
sametweb <sametmutevellioglu@gmail.com>
2019-12-19 19:16:34 -0800

    Component structure and files created, React Router implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import Login from "./components/Login";
+import SubmitStory from "./components/SubmitStory";
+import AdminPortal from "./components/AdminPortal";
+import Story from "./components/Story";
+import Footer from "./components/Footer";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Route exact path="/" component={Homepage} />
+      <Route path="/login" component={Login} />
+      <Route path="/submit-story" component={SubmitStory} />
+      <Route path="/admin" component={AdminPortal} />
+      {/* This will be a private route! */}
+      <Route path="/story/:id" component={Story} />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
