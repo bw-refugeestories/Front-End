be6d367fcf0f3a45d4cd2c66986beb6dbd3fdbf1
sametweb <sametmutevellioglu@gmail.com>
2020-01-06 23:53:00 -0800

    Single story pages work independently, Carousel items fetched dynamically, minor styling fixes.

--- src/components/Homepage/StoryCard.js
+++ src/components/Homepage/StoryCard.js
@@ -5,7 +5,7 @@ import { Col, Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
 const StoryCard = ({ story }) => {
   return (
     <Col xs={12} md={4} className="my-3">
-      <Card className="rounded-0 border-0 story-card">
+      <Card className="rounded-0 border-0 h-100 story-card">
         <CardImg
           top
           width="100%"
@@ -18,10 +18,7 @@ const StoryCard = ({ story }) => {
             {story.storyContent.substr(0, 140)}...
           </CardText>
           <Link
-            to={{
-              pathname: `/story/${story.id}`,
-              state: { story }
-            }}
+            to={`/story/${story.id}`}
             className="btn btn-outline-secondary stretched-link"
           >
             Read More &rarr;
