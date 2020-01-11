61a96b596c005a36fbc8cf2daf7c0665908b6fb7
sametweb <sametmutevellioglu@gmail.com>
2020-01-11 00:35:51 -0800

    Pending stories page says 'no pending stories' if there is not any.

--- src/components/AdminPortal.js
+++ src/components/AdminPortal.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Button from "@material-ui/core/Button";
 import { fetch_pending_stories } from "../utils/actions";
 import PendingStory from "./PendingStory.js";
 
-const AdminPortal = props => {
+const AdminPortal = ({ pendingStories, fetch_pending_stories }) => {
   useEffect(() => {
-    props.fetch_pending_stories();
+    fetch_pending_stories();
   }, []);
-  
+
   return (
     <div className="admin-portal">
       <Link to="/admin/add-user">
@@ -23,11 +22,13 @@ const AdminPortal = props => {
           Modify User
         </Button>
       </Link>
-      <h1>Pending Stories</h1>
+      <h2>Pending Stories</h2>
       <div className="pending-wrapper">
-        {props.pendingStories.map(story => (
-          <PendingStory story={story} key={story.id} storyId={story.id} />
-        ))}
+        {pendingStories.length > 0
+          ? pendingStories.map(story => (
+              <PendingStory story={story} key={story.id} storyId={story.id} />
+            ))
+          : "No pending stories."}
       </div>
     </div>
   );
