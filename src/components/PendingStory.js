24c3d2466ed84dab3bcbca9c41fda61d2f5385b1
sametweb <sametmutevellioglu@gmail.com>
2020-01-12 15:24:41 -0800

    Admin portal/pending stories styling improved

--- src/components/PendingStory.js
+++ src/components/PendingStory.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { approveStory, denyStory, fetch_pending_stories, fetch_stories} from '../utils/actions';
+import { connect } from "react-redux";
+import {
+  approveStory,
+  denyStory,
+  fetch_pending_stories,
+  fetch_stories
+} from "../utils/actions";
 
-// Material UI Imports
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import Button from "@material-ui/core/Button";
+import {
+  makeStyles,
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogContentText as P,
+  DialogActions,
+  Button
+} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -15,6 +29,16 @@ const useStyles = makeStyles(theme => ({
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3)
+  },
+  storyButton: {
+    marginBottom: theme.spacing(1),
+    fontSize: theme.spacing(2)
+  },
+  card: {
+    maxWidth: "100%"
+  },
+  media: {
+    height: 140
   }
 }));
 
@@ -31,64 +55,123 @@ const PendingStory = props => {
   };
 
   return (
-    <div>
-      <button type="button" onClick={handleOpen}>
-        {props.story.storyName}
-      </button>
-      <Modal
+    <React.Fragment>
+      {/* <Button
+            className={classes.storyButton}
+            fullWidth
+            variant="outlined"
+            onClick={handleOpen}
+          >
+            {props.story.storyName}
+          </Button> */}
+      <Card className={classes.card} onClick={handleOpen}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.story.storyName}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {props.story.storyContent.split(".")[0]}...
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll="paper"
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">
+          {props.story.storyName}
+        </DialogTitle>
+        <DialogContent dividers={true}>
+          {props.story.storyContent.split("\n").map((p, i) => (
+            <P key={i}>{p}</P>
+          ))}
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              const updateStories = {
+                ...props.story,
+                approved: true
+              };
+              props.approveStory(updateStories);
+              handleClose();
+            }}
+          >
+            Accept
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              props.denyStory(props.story.id);
+              handleClose();
+            }}
+          >
+            Deny
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* <Modal
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
         open={open}
         onClose={handleClose}
       >
-        <div className={classes.paper}>
+        <div style={modalStyle} className={classes.paper}>
           <h2 id="simple-modal-title">{props.story.storyName}</h2>
           <p id="simple-modal-description">
-            {props.story.storyContent}
+            {props.story.storyContent.split("\n").map(p => (
+              <p>{p}</p>
+            ))}
             <br></br>
-            <Button 
-            variant="contained" 
-            color="primary"
-            onClick={() => {
-                const updateStories ={
-                    ...props.story,
-                    approved: true,
-                }
-                console.log(updateStories)
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                const updateStories = {
+                  ...props.story,
+                  approved: true
+                };
                 props.approveStory(updateStories);
                 handleClose();
-            }}
+              }}
             >
               Accept
             </Button>
-            <Button 
-            variant="contained" 
-            color="secondary"
-            onClick={() => {
-                props.denyStory(props.story.id)
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                props.denyStory(props.story.id);
                 handleClose();
-            }}
+              }}
             >
               Deny
             </Button>
           </p>
         </div>
-      </Modal>
-    </div>
+      </Modal>*/}
+    </React.Fragment>
   );
 };
 
 const mapStateToProps = state => {
-    return {
-        storyReducer: state.storyReducer
-    }
-}
+  return {
+    storyReducer: state.storyReducer
+  };
+};
 
-export default connect (
-    mapStateToProps, {
-        approveStory,
-        denyStory,
-        fetch_stories,
-        fetch_pending_stories
-    }
-) (PendingStory);
+export default connect(mapStateToProps, {
+  approveStory,
+  denyStory,
+  fetch_stories,
+  fetch_pending_stories
+})(PendingStory);
